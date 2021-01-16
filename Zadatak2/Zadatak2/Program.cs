e536c181f0a2d3cc70c9ee1588d19a9e3633333a
UltraXDZN <ultraxdzn@gmail.com>
2021-01-16 17:58:56 +0100

    Rjesena prva 3 zadatka
    - rjesen zadatak 1
    - rjesen zadatak 2
    - rjesen zadatak 3

---
+++ Zadatak2/Zadatak2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite broj: ");
+            float broj = int.Parse(Console.ReadLine());
+            Dictionary<string, bool> djeljivost = new Dictionary<string, bool>();
+            djeljivost.Add("Broj je djeljiv s 4", broj % 4 == 0);
+            djeljivost.Add("Broj je djeljiv s 6", broj % 6 == 0);
+            djeljivost.Add("Broj je djeljiv sa 4 i sa 6", broj % 4 == 0 && broj % 6 == 0);
+            djeljivost.Add("Broj nije djeljiv sa 4 ni sa 6", broj % 4 != 0 && broj % 6 != 0);
+            foreach (var kv in djeljivost) if (kv.Value) Console.WriteLine(kv.Key);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadatak3/Zadatak3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i <= 39; i++)
+            {
+                for (int j = 1; j <= 39; j++)
+                {
+                    if (i == j) continue;
+                    for (int k = 1; k <= 39; k++)
+                    {
+                        if (j == k || k == i) continue;
+                        for (int l = 1; l <= 39; l++)
+                        {
+                            if (k == l || l == j || l == i) continue;
+                            for (int m = 1; m <= 39; m++)
+                            {
+                                if (m == l || m == j || m == k || m == i) continue;
+                                for (int n = 1; n <= 39; n++)
+                                {
+                                    if (m == n || n == l || n == k || n == j || n == i) continue;
+                                    for (int o = 1; o <= 39; o++)
+                                    {
+                                        if (n == o || n == m || n == l || n == k || n == j || n == i) continue;
+                                        Console.WriteLine($"{i} {j} {k} {l} {m} {n} {o}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
